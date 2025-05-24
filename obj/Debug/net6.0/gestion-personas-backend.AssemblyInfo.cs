d4be1612264779915328fcdd3c1f1ecbbacaf779
braduoc <bra.chacon@duocuc.cl>
2025-05-24 18:09:24 -0400

    añadiendo nuget.config

--- obj/Debug/net6.0/gestion-personas-backend.AssemblyInfo.cs
+++ obj/Debug/net6.0/gestion-personas-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("gestion-personas-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd4a44fa2956b73e365a4139a4dc71b89112f4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fda461e5e5cb0486cddb9ba97c1a7b6358ed3eae")]
 [assembly: System.Reflection.AssemblyProductAttribute("gestion-personas-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("gestion-personas-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
